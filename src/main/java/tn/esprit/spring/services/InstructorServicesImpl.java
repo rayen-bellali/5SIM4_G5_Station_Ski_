a216a3365af27b37e7dceae353005262b6942d0d
AmiraBenMbarek <amira.benmnbarek@esprit.tn>
2024-11-08 01:43:17 +0100

    added target directory and log4j

--- src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
@@ -10,42 +10,51 @@ import tn.esprit.spring.repositories.IInstructorRepository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @AllArgsConstructor
 @Service
 public class InstructorServicesImpl implements IInstructorServices{
+    private static final Logger logger = LogManager.getLogger(InstructorServicesImpl.class);
 
     private IInstructorRepository instructorRepository;
     private ICourseRepository courseRepository;
 
     @Override
     public Instructor addInstructor(Instructor instructor) {
+        logger.info("Adding instructor: {}", instructor);
         return instructorRepository.save(instructor);
     }
 
     @Override
     public List<Instructor> retrieveAllInstructors() {
+        logger.info("Retrieving all instructors");
         return instructorRepository.findAll();
     }
 
     @Override
     public Instructor updateInstructor(Instructor instructor) {
+        logger.info("Updating instructor: {}", instructor);
         return instructorRepository.save(instructor);
     }
 
     @Override
     public Instructor retrieveInstructor(Long numInstructor) {
+        logger.info("Retrieving instructor with id: {}", numInstructor);
         return instructorRepository.findById(numInstructor).orElse(null);
     }
 
     @Override
     public Instructor addInstructorAndAssignToCourse(Instructor instructor, Long numCourse) {
+        logger.info("Adding instructor: {} and assigning to course with id: {}", instructor, numCourse);
         Course course = courseRepository.findById(numCourse).orElse(null);
-        Set<Course> courseSet = new HashSet<>();
-        courseSet.add(course);
-        instructor.setCourses(courseSet);
-        return instructorRepository.save(instructor);
+        if (course != null) {
+            Set<Course> courseSet = new HashSet<>();
+            courseSet.add(course);
+            instructor.setCourses(courseSet);
+            return instructorRepository.save(instructor);
+        }
+        return null;
     }
-
-
 }
