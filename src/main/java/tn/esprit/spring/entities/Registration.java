2cf452f1f90ef24df9146d4fcd71b6d42bae6ff9
moniajebabli <monia.jebabli@esprit.tn>
2024-11-08 14:46:11 +0100

    modifaction 2

--- src/main/java/tn/esprit/spring/entities/Registration.java
+++ src/main/java/tn/esprit/spring/entities/Registration.java
@@ -1,12 +1,12 @@
 package tn.esprit.spring.entities;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
@@ -16,6 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
+
 @Getter
 @Setter
 @ToString
@@ -28,11 +29,14 @@ public class Registration implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	Long numRegistration;
+
+	@Min(value = 1, message = "Le numéro de semaine doit être au moins 1")
 	int numWeek;
 
 	@JsonIgnore
 	@ManyToOne
-    Skier skier;
+	Skier skier;
+
 	@JsonIgnore
 	@ManyToOne
 	Course course;
